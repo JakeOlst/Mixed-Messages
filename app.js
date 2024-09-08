8029191088caed8b7c594c49e4bfeb0e20cfafd7
JakeOlst <jstainthorpe123@gmail.com>
2024-09-08 15:16:12 +0100

    Added initial code for Mixed Messages

---
+++ app.js
@@ -0,0 +1,9 @@
+let messages = [
+    "Test Message 1",
+    "Test Message 2",
+    "Test Message 3"
+];
+
+let randomMessage = messages[Math.floor(Math.random() * messages.length)];
+
+console.log(randomMessage);
